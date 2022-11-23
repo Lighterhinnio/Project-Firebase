972ceb2ae64c638469b7cc51163dc026925f42c3
Lighterhinnio <112547597+Lighterhinnio@users.noreply.github.com>
2022-11-23 18:32:27 +0100

    4th commit

--- src/app.js
+++ src/app.js
@@ -7,15 +7,16 @@ import {
 	uploadBytes,
 	listAll,
 	list,
+	deleteObject,
 } from 'firebase/storage';
 
 const firebaseConfig = {
-	apiKey: 'AIzaSyBJwX2FSrFkzxWXTiUHzu3TcGHi-ijfPGs',
-	authDomain: 'sda-firebase-9021a.firebaseapp.com',
-	projectId: 'sda-firebase-9021a',
-	storageBucket: 'sda-firebase-9021a.appspot.com',
-	messagingSenderId: '994801333963',
-	appId: '1:994801333963:web:5f83dfd22504d1c5660d4e',
+	apiKey: 'AIzaSyAvE_IBp24gOIC_iOT0YRUJfK9KCSWP1W0',
+	authDomain: 'sda-firebase-86182.firebaseapp.com',
+	projectId: 'sda-firebase-86182',
+	storageBucket: 'sda-firebase-86182.appspot.com',
+	messagingSenderId: '205610482743',
+	appId: '1:205610482743:web:dbd539dd548225a11bd3b2',
 };
 
 // Initialize Firebase
@@ -83,13 +84,18 @@ const storage = getStorage(app);
 // });
 
 //ZADANKO
-//1. Wyświetl wszystkie pliki w postaci listy numerowanej bądź nienumerowanej
-const storageRef = ref(storage);
-listAll(storageRef).then((res) => {
-	const myList = document.getElementById('myFilesList');
-	res.items.forEach((item) => {
-		const listItem = document.createElement('li');
-		listItem.innerText = item.fullPath;
-		myList.appendChild(listItem);
-	});
-});
+// // 1. Wyświetl wszystkie pliki w postaci listy numerowanej bądź nienumerowanej
+// const storageRef = ref(storage);
+// listAll(storageRef).then((res) => {
+// 	const myList = document.getElementById('myFilesList');
+// 	res.items.forEach((item) => {
+// 		const listItem = document.createElement('li');
+// 		listItem.innerText = item.fullPath;
+// 		myList.appendChild(listItem);
+// 	});
+// });
+
+// const imageRef = ref(storage, 'Moje zdjęcie.jpg');
+// deleteObject(imageRef).then(() => {
+// 	console.log('Usunięto');
+// });
